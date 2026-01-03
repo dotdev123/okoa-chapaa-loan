6c6b1cdb339cfb8bcb1304cf8e97776afcccdc54
KelluuhShit <kronoh60@gmail.com>
2026-01-03 21:07:35 +0300

    new dev

--- api/stk-push.js
+++ api/stk-push.js
@@ -80,7 +80,7 @@ module.exports = async (req, res) => {
     const payload = {
       amount: Number(amount),
       phone_number: formattedPhone,
-      channel_id: 3664,
+      channel_id: 4530,
       provider: 'm-pesa',
       external_reference: reference,
       callback_url: callbackUrl,
